5bab658df3714fa17653f56da7ad55eb4e63a9c1
Pegas13fps <Tymoschenkob@gmail.com>
2018-11-05 15:43:05 +0200

    New proj push for correcting

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("registration-form").onclick = function() {
+	document.getElementById("form-box").style.display = "block";
+}
+document.getElementById("registration-form2").onclick = function() {
+	document.getElementById("form-box").style.display = "block";
+}
